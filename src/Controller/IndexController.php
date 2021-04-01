372f79f45f8c09094a5be754c5c6d3b4a6be4787
Carlos Aguiar <carlosaguiar23@gmail.com>
2021-04-01 02:48:38 -0300

    creatting user authentication

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/home", name="home")
      * @return Response
      */
     public function index(): Response
